34d2d64f891a50689947e092d6f3c6ecceb97235
Tornike Kapanadze <tornikekapan@gmail.com>
2025-01-14 19:55:04 +0200

    Update src/utils/generateUniqueId.js
    Co-authored-by: Eugene Glova <eugene.glova@gmail.com>

--- src/utils/generateUniqueId.js
+++ src/utils/generateUniqueId.js
@@ -1,8 +1,6 @@
 /**
  * @returns {string}
  */
-export const generateUniqueId = () => {
-  return (
-    new Date().getTime().toString(36) + Math.random().toString(36).substring(2)
-  )
-}
+export const generateUniqueId = () => (
+  new Date().getTime().toString(36) + Math.random().toString(36).substring(2)
+)
