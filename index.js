fa5c28446dc0dabf99891963adb6353f943ff2b5
Tornike <tornikekapan@gmail.com>
2025-02-16 22:45:09 +0200

    feat: update dependencies

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import { i18n } from '@lingui/core'
 import { I18nProvider } from '@lingui/react'
+import Autopass from 'autopass'
+import Corestore from 'corestore'
 import { html } from 'htm/react'
 import { ThemeProvider } from 'pearpass-lib-ui-theme-provider'
-import { setStoragePath, VaultProvider } from 'pearpass-lib-vault'
+import {
+  closeActiveVaultInstance,
+  closeVaultsInstance,
+  setAutoPassInstance,
+  setCoreStoreInstance,
+  setStoragePath,
+  VaultProvider
+} from 'pearpass-lib-vault'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './src/containers/App'
@@ -11,7 +20,13 @@ import { RouterProvider } from './src/context/RouterContext'
 import { messages } from './src/locales/en/messages.mjs'
 import { setFontsAndResetCSS } from './styles'
 
-Pear.updates(() => Pear.reload())
+Pear.updates(async () => {
+  await closeActiveVaultInstance?.()
+
+  await closeVaultsInstance?.()
+
+  Pear.reload()
+})
 
 const root = createRoot(document.querySelector('#root'))
 
@@ -21,6 +36,8 @@ i18n.load('en', messages)
 i18n.activate('en')
 
 setStoragePath(Pear.config.storage)
+setAutoPassInstance(Autopass)
+setCoreStoreInstance(Corestore)
 
 root.render(html`
   <${VaultProvider}>
