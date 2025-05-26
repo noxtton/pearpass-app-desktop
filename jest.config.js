6a24e03a09f66d21e404164e27f476cfdfbddf23
Giorgi Shavtvalishvili <gshavtvalishvili@gmail.com>
2025-05-26 13:39:46 +0400

    chore: add yalc files to jest config testPathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,10 @@ export default {
     '^pearpass-lib-ui-theme-provider$':
       '<rootDir>/node_modules/pearpass-lib-ui-theme-provider/src/index.js'
   },
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.yalc/'
+  ],
   transformIgnorePatterns: [
     'node_modules/(?!(htm|pearpass-lib-ui-theme-provider|pearpass-lib-ui-react-components|pear-apps-lib-ui-react-hooks|pear-apps-utils-validator|pearpass-lib-vault|pearpass-lib-vault-desktop|pearpass-utils-password-check|pearpass-utils-password-generator|pear-apps-utils-pattern-search|pear-apps-utils-avatar-initials|pear-apps-lib-feedback)/)'
   ]
