67b7a8f3124d3f01a4a8577fb0cfd70b8f3714db
Tornike Kapanadze <tornikekapan@gmail.com>
2025-08-08 17:34:44 +0300

    fix: animation soud improvements

--- src/pages/Intro/PasswordFillAnimation/index.js
+++ src/pages/Intro/PasswordFillAnimation/index.js
@@ -1,5 +1,3 @@
-import { useEffect } from 'react'
-
 import { RuntimeLoader } from '@rive-app/canvas'
 import { useRive } from '@rive-app/react-canvas'
 import { html } from 'htm/react'
@@ -7,25 +5,14 @@ import { html } from 'htm/react'
 RuntimeLoader.setWasmUrl('assets/rive/rive.wasm')
 
 export const PasswordFillAnimation = () => {
-  const { RiveComponent, rive } = useRive({
+  const { RiveComponent } = useRive({
     src: 'assets/animations/pearpass_password.riv',
-    autoplay: false,
-    animations: ['Timeline 1', 'Timeline 2']
+    stateMachines: ['State Machine 1'],
+    animations: ['Timeline 1', 'Timeline 2'],
+    autoplay: true
   })
 
-  useEffect(() => {
-    if (rive && rive.loaded) {
-      const names = rive.animationNames
-
-      if (names.length > 0) {
-        rive.play(names[0])
-      }
-    }
-  }, [rive])
-
   return html`<${RiveComponent}
-    onMouseEnter=${() => rive && rive.play()}
-    onMouseLeave=${() => rive && rive.pause()}
     style=${{ width: '100%', aspectRatio: '1 / 1' }}
   />`
 }
