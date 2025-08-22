10b34d9f3d69003bf32a9527a7eccd7d68c64d84
Tornike Kapanadze <tornikekapan@gmail.com>
2025-08-22 12:48:30 +0300

    fix: redirect improvements

--- src/app/App/hooks/useRedirect.js
+++ src/app/App/hooks/useRedirect.js
@@ -1,43 +1,51 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { useUserData } from 'pearpass-lib-vault'
 
 import { LOCAL_STORAGE_KEYS } from '../../../constants/localStorage'
 import { useRouter } from '../../../context/RouterContext'
+import { logger } from '../../../utils/logger'
 
 /**
  * @returns {Object} An object containing:
  * @property {boolean} isLoading - Indicates if the user data is currently loading.
  */
 export const useRedirect = () => {
+  const [isLoading, setIsLoading] = useState(true)
+
   const { navigate } = useRouter()
 
-  const { isLoading: isUserDataLoading, refetch: refetchUser } = useUserData()
-
-  const isLoading = isUserDataLoading
+  const { refetch: refetchUser } = useUserData()
 
   useEffect(() => {
     ;(async () => {
-      const userData = await refetchUser()
+      try {
+        setIsLoading(true)
+        const userData = await refetchUser()
 
-      if (!userData?.hasPasswordSet) {
-        navigate('intro')
-        return
+        if (!userData?.hasPasswordSet) {
+          navigate('intro')
+          return
+        }
+
+        const isTouAccepted =
+          localStorage.getItem(LOCAL_STORAGE_KEYS.TOU_ACCEPTED) === 'true'
+
+        if (!isTouAccepted) {
+          navigate('termsOfUse')
+          return
+        }
+
+        navigate('welcome', {
+          state: userData?.hasPasswordSet
+            ? 'masterPassword'
+            : 'createMasterPassword'
+        })
+      } catch (error) {
+        logger.error('Error fetching user data:', error)
+      } finally {
+        setIsLoading(false)
       }
-
-      const isTouAccepted =
-        localStorage.getItem(LOCAL_STORAGE_KEYS.TOU_ACCEPTED) === 'true'
-
-      if (!isTouAccepted) {
-        navigate('termsOfUse')
-        return
-      }
-
-      navigate('welcome', {
-        state: userData?.hasPasswordSet
-          ? 'masterPassword'
-          : 'createMasterPassword'
-      })
     })()
   }, [])
 
