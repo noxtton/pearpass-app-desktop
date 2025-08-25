4e9cf814de6ed480d1a07cd03c9c5b264d8bc9a8
ndolidze <n.dolidze@noxtton.com>
2025-08-25 17:47:36 +0400

    feat/93: Add copy to clipboard opt-in

--- src/components/SwitchWithLabel/styles.js
+++ src/components/SwitchWithLabel/styles.js
@@ -12,6 +12,12 @@ export const Wrapper = styled.div`
   cursor: pointer;
 `
 
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
+
 export const Label = styled.div.withConfig({
   shouldForwardProp: (prop) => !['isBold'].includes(prop)
 })`
@@ -19,4 +25,13 @@ export const Label = styled.div.withConfig({
   font-family: 'Inter';
   font-size: 14px;
   font-weight: ${({ isBold }) => (isBold ? '600' : '400')};
+`
+
+export const Description = styled.div`
+  color: ${({ theme }) => theme.colors.white.mode1};
+  font-family: 'Inter';
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
 `
