89f004bf176fbdb60ac91eed79169a02c6b1d63c
ndolidze <n.dolidze@noxtton.com>
2025-09-26 00:35:25 +0400

    55/fix: Add custom hook for extension exit

--- src/app/App/index.js
+++ src/app/App/index.js
@@ -1,12 +1,10 @@
-import { useEffect } from 'react'
-
 import { html } from 'htm/react'
 
-import { useRouter } from '../../context/RouterContext'
 import { usePearUpdate } from '../../hooks/usePearUpdate'
 import { useSimulatedLoading } from '../../hooks/useSimulatedLoading'
 import { Routes } from '../Routes'
 import { useInactivity } from './hooks/useInactivity'
+import { UseOnExtensionExit } from './hooks/useOnExtensionExit'
 import { useRedirect } from './hooks/useRedirect'
 
 export const App = () => {
@@ -16,18 +14,7 @@ export const App = () => {
   useInactivity()
   const { isLoading } = useRedirect()
 
-  const { navigate } = useRouter()
-
-  useEffect(() => {
-    const handleExtensionExit = async () => {
-      navigate('welcome', { state: 'masterPassword' })
-    }
-
-    window.addEventListener('extension-exit', handleExtensionExit)
-    return () => {
-      window.removeEventListener('extension-exit', handleExtensionExit)
-    }
-  }, [navigate])
+  UseOnExtensionExit()
 
   return html` <${Routes} isLoading=${isLoading || isSimulatedLoading} /> `
 }
